c6689e7ec08a68ad768a1a62a5703368eb66dbc9
chenbihao <994523036@qq.com>
2020-12-06 22:02:54 +0800

    集成druid

--- myroad-sys/src/test/java/cn/noobbb/myroadsys/controller/HelloTest.java
+++ myroad-sys/src/test/java/cn/noobbb/myroadsys/controller/HelloTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -23,6 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  */
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class HelloTest {
     @Resource
     private MockMvc mvc;
