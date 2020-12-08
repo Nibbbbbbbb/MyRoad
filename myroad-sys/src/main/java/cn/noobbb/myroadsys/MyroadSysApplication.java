ee06691bf246ad7459c8b3b07f96ac9ef978f91d
chenbihao <994523036@qq.com>
2020-12-08 21:18:32 +0800

    优化pom

--- myroad-sys/src/main/java/cn/noobbb/myroadsys/MyroadSysApplication.java
+++ myroad-sys/src/main/java/cn/noobbb/myroadsys/MyroadSysApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyroadSysApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MyroadSysApplication.class, args);
+		System.out.println("系统启动成功\n");
+
 	}
 
 }
