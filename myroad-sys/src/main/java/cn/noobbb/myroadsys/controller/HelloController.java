5f0dd5c1072ea6f1324b4676cc4afb1996b6b2d3
chenbihao <994523036@qq.com>
2020-12-18 22:22:09 +0800

    引入Swagger和Knife4j

--- myroad-sys/src/main/java/cn/noobbb/myroadsys/controller/HelloController.java
+++ myroad-sys/src/main/java/cn/noobbb/myroadsys/controller/HelloController.java
@@ -1,6 +1,9 @@
 package cn.noobbb.myroadsys.controller;
 
 import cn.noobbb.myroadutils.hello.HelloUtils;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @Description:
  * @History:
  */
+@Api(tags = "hello模块")
 @RestController
 @RequestMapping("/sys")
 public class HelloController {
@@ -21,6 +25,8 @@ public class HelloController {
     @Value("${cn.noobbb.title}")
     String blogTitle;
 
+
+    @ApiOperation(value = "hello")
     @GetMapping(value = "hello")
     public String hello() {
         return HelloUtils.getHello() + ",This is my blog," + blogName + " " + blogTitle;
